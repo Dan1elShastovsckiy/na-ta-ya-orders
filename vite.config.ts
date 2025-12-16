fd057d560c274eab282354d70e57aecb086b0eba
Daniel <44569206+Dan1elShastovsckiy@users.noreply.github.com>
2025-12-16 14:15:10 +0300

    Сборка и инструкции для поддомена новые

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages relative paths
+  // Change base to '/' for proper subdomain hosting on Nginx
+  base: '/', 
   build: {
     outDir: 'dist',
   }
